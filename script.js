e3157c3518033507148f45c32780e3ae1bd2ac96
afreen485 <shaikjilekha184@gmail.com>
2026-05-28 19:00:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+/* ================= OPTIONAL JAVASCRIPT ================= */
+
+/* Smooth animation on scroll */
+
+window.addEventListener("scroll",()=>{
+
+    const cards =
+    document.querySelectorAll(".card");
+
+    cards.forEach(card=>{
+
+        const cardTop =
+        card.getBoundingClientRect().top;
+
+        if(cardTop < window.innerHeight - 50){
+
+            card.style.opacity = "1";
+            card.style.transform = "translateY(0px)";
+        }
+
+    });
+
+});
